924147c398b12c6da22b3422c7b500d9b9e66bd0
Serhii Klimushyn <seroga2801@gmail.com>
2022-02-16 10:44:20 +0200

    add postgresql

--- main/settings.py
+++ main/settings.py
@@ -79,13 +79,22 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 LOGIN_URL = reverse_lazy("user:sign-in")
 LOGOUT_REDIRECT_URL = reverse_lazy("user:sign-in")
 # Password validation
